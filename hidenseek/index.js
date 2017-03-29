663e1d5e4aa34b630cf1287a8788746d080422f8
rkkmkkfx <baranovs-v@mail.ru>
2017-03-29 21:26:15 +0800

    1.2 «Модули и работа с файлами»

--- hidenseek/index.js
+++ hidenseek/index.js
@@ -13,7 +13,10 @@ const POKEMONS_LIST = new PokemonList(...POKEMONS);
 
 const PATH = 'field';
 
-if (!process.argv[3]) {
+hidenseek.hide(PATH, POKEMONS_LIST)
+    .then(() => hidenseek.seek(PATH));
+
+/*if (!process.argv[3]) {
     hidenseek.hide(PATH, POKEMONS_LIST)
         .then(() => hidenseek.seek(PATH));
 } else if (process.argv[3]) {
@@ -21,4 +24,4 @@ if (!process.argv[3]) {
     if (process.argv[3] === 'seek') {
         hidenseek[process.argv[3]](PATH);
     }
-}
+}*/
