e228db7b920d9cc7f5ae34c50cb46ee353752089
rkkmkkfx <baranovs-v@mail.ru>
2017-05-20 15:08:08 +0800

    4.3 «XHR-запросы в Angular - основа работы с back-end приложения»

--- 4.3-xhr/app/src/PokemonsService/PokemonsService.js
+++ 4.3-xhr/app/src/PokemonsService/PokemonsService.js
@@ -17,8 +17,8 @@ angular
                         method: 'POST',
                         url: 'https://api.backendless.com/v1/data/pokemon',
                         headers: {
-                            "application-id": "6F3C11F6-A0FC-C66C-FFCA-E619CDC6BF00",
-                            "secret-key": "DB668E23-F92D-0841-FF13-5FCD68136700"
+                            "application-id": "D87AA1BE-B938-22D6-FFCF-86452D634100",
+                            "secret-key": "D9159766-80D3-3213-FF41-3FE21EC7A400"
 
                         },
                         data: pokemonData
